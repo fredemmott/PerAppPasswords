bd7bb1448ad12d821e20b87098186e6134a0751f
Fred <mail@fredemmott.co.uk>
2011-08-14 15:42:45 +0000

    add google authenticator support
    Requires ga-verifyd to be running (see ga_verify gem)

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.0.9'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'ga_verify', '~> 0.0.1'
 gem 'mysql2', '~> 0.2.0'
 gem 'sqlite3'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,26 @@
+require 'ga_verify/client'
 class HomeController < ApplicationController
   def index
     authenticate_with_http_basic do |user,password|
-      # TODO: GOOGLE AUTH
-      session[:user] = user
-      redirect_to :controller => :passwords, :method => :index
+      @code = params[:otpcode].to_i || nil
+
+      begin
+        @result = GAVerify::Client.new.check_user(user, @code)
+      rescue Thrift::TransportException
+        # No server, or server is down
+        @result = GAVerify::Result::NO_GOOGLE_AUTH
+      end
+
+      passable = [
+        GAVerify::Result::SUCCESS,
+        GAVerify::Result::NO_GOOGLE_AUTH
+      ]
+      if passable.include? @result
+        session[:user] = user
+        redirect_to :controller => :passwords, :method => :index
+      else
+        render
+      end
     end
   end
 end
